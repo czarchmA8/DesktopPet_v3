d697be413ec9d938ba0ee4f0c2d222a3a23c3919
Shouryaverma19 <shouryavermashourya@gmail.com>
2026-07-06 16:22:36 +0530

    Update Hindi translations in hi.ts file

--- languages/hi.ts
+++ languages/hi.ts
@@ -1,23 +1,458 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="hi_IN">
+<TS version="2.1" language="hi_IN" sourcelanguage="en_US">
 <context>
-    <name>Dashboard</name>
+    <name>AboutDialog</name>
     <message>
+        <location filename="../dashboard.py" line="909"/>
+        <source>DesktopPet_v3</source>
+        <translation>DesktopPet_v3</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="910"/>
+        <source>Description DesktopPet_v3</source>
+        <translation>&lt;b&gt;डेस्कटॉप पेट v3&lt;/b&gt;&lt;br&gt;
+&lt;br&gt;
+एक इंटरैक्टिव डेस्कटॉप पेट एप्लिकेशन जिसमें भौतिकी सिमुलेशन, नियंत्रण कक्ष और परिष्कृत सिस्टम विंडो व्यवहार शामिल हैं।&lt;br&gt;
+&lt;br&gt;
+संस्करण: %app_version&lt;br&gt;
+रिपॉजिटरी: &lt;a href=&apos;https://github.com/czarchmA8/DesktopPet_v3&apos; style=&apos;color: #0d6efd;&apos;&gt;czarchmA8/DesktopPet_v3&lt;/a&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>ControlWindow</name>
+    <message>
+        <location filename="../dashboard.py" line="319"/>
+        <source>DesktopPet_v3</source>
+        <comment>App title</comment>
+        <translation>DesktopPet_v3</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="358"/>
+        <source>Statistics</source>
+        <comment>Tab text</comment>
+        <translation>आंकड़े</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="360"/>
         <source>Settings</source>
+        <comment>Tab text</comment>
         <translation>सेटिंग्स</translation>
     </message>
     <message>
-        <source>Close</source>
-        <translation>बंद करें</translation>
+        <location filename="../dashboard.py" line="362"/>
+        <source>Objects</source>
+        <comment>Tab text</comment>
+        <translation>ऑब्जेक्ट्स</translation>
     </message>
     <message>
+        <location filename="../dashboard.py" line="377"/>
+        <source>Version %x</source>
+        <translation>संस्करण %x</translation>
+    </message>
+</context>
+<context>
+    <name>HotkeyDialog</name>
+    <message>
+        <location filename="../dashboard.py" line="241"/>
+        <source>Record keyboard shortcut</source>
+        <translation>कीबोर्ड शॉर्टकट रिकॉर्ड करें</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="246"/>
+        <source>Press key combination:</source>
+        <translation>कुंजी संयोजन दबाएं:</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="258"/>
+        <source>OK</source>
+        <translation>ठीक है</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="261"/>
+        <source>Cancel</source>
+        <translation>रद्द करें</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="693"/>
+        <location filename="../dashboard.py" line="704"/>
+        <source>Success</source>
+        <translation>सफलता</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="693"/>
+        <source>Assigned &apos;%x&apos;.</source>
+        <translation>&apos;%x&apos; असाइन किया गया।</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="702"/>
+        <source>None</source>
+        <translation>कोई नहीं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="704"/>
+        <source>Shortcut removed.</source>
+        <translation>शॉर्टकट हटा दिया गया।</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="706"/>
+        <source>Information</source>
+        <translation>जानकारी</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="706"/>
+        <source>No shortcut assigned.</source>
+        <translation>कोई शॉर्टकट असाइन नहीं है।</translation>
+    </message>
+</context>
+<context>
+    <name>KeySequence</name>
+    <message>
+        <location filename="../dashboard.py" line="167"/>
+        <source>Press key combination...</source>
+        <translation>कुंजी संयोजन दबाएं...</translation>
+    </message>
+</context>
+<context>
+    <name>Objects</name>
+    <message>
+        <location filename="../dashboard.py" line="789"/>
+        <source>Create Object (Cursor)</source>
+        <translation>ऑब्जेक्ट बनाएं (कर्सर)</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="794"/>
+        <source>Assign keyboard shortcut</source>
+        <translation>कीबोर्ड शॉर्टकट असाइन करें</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="799"/>
+        <source>Remove shortcut</source>
+        <translation>शॉर्टकट हटाएं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="809"/>
+        <source>🗑 Delete all objects</source>
+        <translation>🗑 सभी ऑब्जेक्ट्स हटाएं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="816"/>
+        <source>Select an object to see a shortcut.</source>
+        <translation>शॉर्टकट देखने के लिए एक ऑब्जेक्ट चुनें।</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="826"/>
+        <location filename="../dashboard.py" line="862"/>
+        <source>No object</source>
+        <translation>कोई ऑब्जेक्ट नहीं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="826"/>
+        <location filename="../dashboard.py" line="862"/>
+        <source>Select an object from the list.</source>
+        <translation>सूची से एक ऑब्जेक्ट चुनें।</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="838"/>
+        <location filename="../dashboard.py" line="880"/>
+        <source>Success</source>
+        <translation>सफलता</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="838"/>
+        <source>Removed shortcut for &apos;%1&apos;</source>
+        <translation>&apos;%1&apos; के लिए शॉर्टकट हटा दिया गया</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="840"/>
+        <source>Information</source>
+        <translation>जानकारी</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="840"/>
+        <source>This object currently has no shortcut assigned.</source>
+        <translation>इस ऑब्जेक्ट के लिए वर्तमान में कोई शॉर्टकट असाइन नहीं है।</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="880"/>
+        <source>&apos;%1&apos; assigned to &apos;%2&apos;</source>
+        <translation>&apos;%1&apos; को &apos;%2&apos; पर असाइन किया गया</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="883"/>
+        <source>Error</source>
+        <translation>त्रुटि</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="883"/>
+        <source>There was a problem assigning &apos;%1&apos; to &apos;%2&apos;</source>
+        <translation>&apos;%1&apos; को &apos;%2&apos; पर असाइन करने में एक समस्या थी</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="890"/>
+        <source>Shortcut:</source>
+        <translation>शॉर्टकट:</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="893"/>
+        <source>No shortcut assigned.</source>
+        <translation>कोई शॉर्टकट असाइन नहीं है।</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../dashboard.py" line="470"/>
+        <source>Sound</source>
+        <comment>settings</comment>
+        <translation>ध्वनि</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="474"/>
+        <source>Volume:</source>
+        <comment>settings</comment>
+        <translation>वॉल्यूम:</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="496"/>
+        <source>System</source>
+        <comment>settings</comment>
+        <translation>सिस्टम</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="500"/>
+        <source>Run at system startup</source>
+        <comment>settings</comment>
+        <translation>सिस्टम स्टार्टअप पर चलाएं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="515"/>
+        <source>Objects</source>
+        <comment>settings</comment>
+        <translation>ऑब्जेक्ट्स</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="519"/>
+        <source>Keyboard shortcut to delete all objects:</source>
+        <comment>settings</comment>
+        <translation>सभी ऑब्जेक्ट्स हटाने के लिए कीबोर्ड शॉर्टकट:</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="525"/>
+        <location filename="../dashboard.py" line="585"/>
+        <location filename="../dashboard.py" line="624"/>
+        <source>No keyboard shortcut</source>
+        <translation>कोई कीबोर्ड शॉर्टकट नहीं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="531"/>
+        <source>Set Shortcut</source>
+        <translation>शॉर्टकट सेट करें</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="534"/>
+        <source>Remove shortcut</source>
+        <translation>शॉर्टकट हटाएं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="546"/>
         <source>Language</source>
+        <comment>settings</comment>
         <translation>भाषा</translation>
     </message>
     <message>
-        <source>Desktop Pet Control Panel</source>
-        <translation>डेस्कटॉप पेट कंट्रोल पैनल</translation>
+        <location filename="../dashboard.py" line="569"/>
+        <source>App</source>
+        <translation>ऐप</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="577"/>
+        <source>Show application</source>
+        <translation>एप्लिकेशन दिखाएं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="578"/>
+        <source>Hide application</source>
+        <translation>एप्लिकेशन छुपाएं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="579"/>
+        <source>Close application</source>
+        <translation>एप्लिकेशन बंद करें</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="591"/>
+        <location filename="../dashboard.py" line="630"/>
+        <source>Set</source>
+        <translation>सेट करें</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="594"/>
+        <location filename="../dashboard.py" line="633"/>
+        <source>Delete</source>
+        <translation>हटाएं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="608"/>
+        <source>Pet</source>
+        <translation>पेट</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="616"/>
+        <source>Show pet</source>
+        <translation>पेट दिखाएं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="617"/>
+        <source>Hide pet</source>
+        <translation>पेट छुपाएं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="618"/>
+        <source>Teleport pet</source>
+        <translation>पेट टेलीपोर्ट करें</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="647"/>
+        <source>Advanced</source>
+        <translation>उन्नत</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="651"/>
+        <source>Debug mode</source>
+        <translation>डीबग मोड</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="657"/>
+        <source>Displaying hitboxes</source>
+        <translation>हिटबॉक्स दिखा रहे हैं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="663"/>
+        <source>Information window</source>
+        <translation>सूचना विंडो</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="722"/>
+        <source>File saving error</source>
+        <translation>फ़ाइल सहेजने में त्रुटि</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="722"/>
+        <source>Failed to save settings: %x</source>
+        <translation>सेटिंग्स सहेजने में विफल: %x</translation>
+    </message>
+</context>
+<context>
+    <name>Statistics</name>
+    <message>
+        <location filename="../dashboard.py" line="389"/>
+        <source>Fitness</source>
+        <comment>statistics</comment>
+        <translation>फिटनेस</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="394"/>
+        <source>Friendship</source>
+        <comment>statistics</comment>
+        <translation>मित्रता</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="399"/>
+        <source>Happiness</source>
+        <comment>statistics</comment>
+        <translation>खुशी</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="404"/>
+        <source>Comfort</source>
+        <comment>statistics</comment>
+        <translation>आराम</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="409"/>
+        <source>Hunger</source>
+        <comment>statistics</comment>
+        <translation>भूख</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="414"/>
+        <source>Thirst</source>
+        <comment>statistics</comment>
+        <translation>प्यास</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="419"/>
+        <source>Energy</source>
+        <comment>statistics</comment>
+        <translation>ऊर्जा</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="424"/>
+        <source>Cleanliness</source>
+        <comment>statistics</comment>
+        <translation>स्वच्छता</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="429"/>
+        <source>Warmth</source>
+        <comment>statistics</comment>
+        <translation>गर्मी</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="434"/>
+        <source>Attention</source>
+        <comment>statistics</comment>
+        <translation>ध्यान</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="439"/>
+        <source>Playfulness</source>
+        <comment>statistics</comment>
+        <translation>चंचलता</translation>
+    </message>
+</context>
+<context>
+    <name>toggle_autostart</name>
+    <message>
+        <location filename="../dashboard.py" line="735"/>
+        <source>Autostart unavailable</source>
+        <translation>ऑटोस्टार्ट अनुपलब्ध</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="735"/>
+        <source>Autostart is only available for the packaged application, not for the script being run.</source>
+        <translation>ऑटोस्टार्ट केवल पैकेज्ड एप्लिकेशन के लिए उपलब्ध है, न कि चलाए जा रहे स्क्रिप्ट के लिए।</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="758"/>
+        <source>Registry error</source>
+        <translation>रजिस्ट्री त्रुटि</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="758"/>
+        <source>Failed to change autostart setting:
+%x</source>
+        <translation>ऑटोस्टार्ट सेटिंग बदलने में विफल:
+%x</translation>
+    </message>
+</context>
+<context>
+    <name>tray-icon</name>
+    <message>
+        <location filename="../dashboard.py" line="932"/>
+        <source>Show Panel</source>
+        <translation>पैनल दिखाएं</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="937"/>
+        <source>About</source>
+        <translation>के बारे में</translation>
+    </message>
+    <message>
+        <location filename="../dashboard.py" line="942"/>
+        <source>Close</source>
+        <translation>बंद करें</translation>
     </message>
 </context>
 </TS>
